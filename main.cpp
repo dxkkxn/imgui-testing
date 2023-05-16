0e8b45a07027b70734165fa065aa2489f98a9ecd
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-05-16 12:58:48 +0200

    hello world working

--- main.cpp
+++ main.cpp
@@ -9,6 +9,16 @@ static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
+void newFrames() {
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+}
+
+void gui() {
+  ImGui::Text("Hello, world %d", 123);
+}
+
 // Main code
 int main(int, char **) {
   glfwSetErrorCallback(glfw_error_callback);
@@ -21,35 +31,36 @@ int main(int, char **) {
   glfwMakeContextCurrent(window);
 
   // Initialize ImGui for GLFW and OpenGL
-  // IMGUI_CHECKVERSION();
-  // ImGui::CreateContext();
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGui::StyleColorsDark();
   // ImGuiIO &io = ImGui::GetIO();
-  // ImGui_ImplGlfw_InitForOpenGL(window, true);
-  // ImGui_ImplOpenGL3_Init()
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init();
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
     // Start a new ImGui frame
-    // ImGui_ImplOpenGL3_NewFrame();
-    // ImGui_ImplGlfw_NewFrame();
-    // ImGui::NewFrame();
+    newFrames();
 
     // Create your ImGui UI here
+    gui();
 
     // Rendering
-    // ImGui::Render();
-    glViewport(0, 0, 500, 500);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
     glClear(GL_COLOR_BUFFER_BIT);
-    // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     glfwSwapBuffers(window);
   }
 
   // Cleanup
-  // ImGui_ImplOpenGL3_Shutdown();
-  // ImGui_ImplGlfw_Shutdown();
-  // ImGui::DestroyContext();
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
   glfwDestroyWindow(window);
   glfwTerminate();
   // ImGui_ImplOpenGL3_Init("#version 330");
