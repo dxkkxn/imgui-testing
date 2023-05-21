60b3b68ffb1c7cd30f4e2752d8e05d881f9a0e6c
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-05-21 23:44:04 +0200

    inspector 1st version

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-#include <cstdio>
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
-#include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
-#include "backends/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <cstdio>
 
 #include <iostream>
+#include <random>
 #include <string>
 #include <vector>
-#include <random>
 
 static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "GLFW Error %d: %s\n", error, description);
@@ -20,38 +19,36 @@ void new_frames() {
   ImGui::NewFrame();
 }
 
-
 struct Point {
-  int x, y, z;
+  float x, y, z;
   // Overload the << operator for Person struct
-  friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+  friend std::ostream &operator<<(std::ostream &os, const Point &p) {
     os << "{ x: " << p.x << ", y: " << p.y << ", z: " << p.z << " }";
     return os;
   }
 };
 
 typedef struct Mesh {
-  const char * string;
+  const char *string;
   std::vector<Point> points;
 
-  friend std::ostream& operator<<(std::ostream& os, const Mesh& m) {
+  friend std::ostream &operator<<(std::ostream &os, const Mesh &m) {
     os << "{ name: " << m.string << ", \n";
-    for (const auto& point : m.points) {
-        os << "point: " << point << ",\n";
+    for (const auto &point : m.points) {
+      os << "point: " << point << ",\n";
     }
     os << "}\n";
     return os;
   }
 } Mesh;
 
-
-Mesh * create_random_mesh(const char * s, int nb_points) {
-  Mesh * m = new Mesh;
+Mesh *create_random_mesh(const char *s, int nb_points) {
+  Mesh *m = new Mesh;
   m->string = s;
   std::random_device rd;
   std::mt19937 gen(rd());
   // Define the range of random numbers
-  std::uniform_int_distribution<int> distribution(1, 100);
+  std::uniform_real_distribution<float> distribution(1, 100);
   // Generate a random number
   // int randomNum = distribution(gen);
   Point p;
@@ -64,13 +61,19 @@ Mesh * create_random_mesh(const char * s, int nb_points) {
   return m;
 };
 
-void my_window(Mesh** meshes, int len) {
+// void inspector_window
+
+
+void my_window(Mesh **meshes, int len) {
   // fix window to left corner
+  static bool object_inspector_active = false;
   float size_y = ImGui::GetIO().DisplaySize.y;
+  if (object_inspector_active) size_y = size_y/2;
+  std::cout << size_y << std::endl;
   ImGui::SetNextWindowPos(ImVec2(0, 0)); // left corner
   ImGui::SetNextWindowSize(ImVec2(300, size_y));
-  // std::cout << ImGui::GetIO().DisplaySize.x << " " << ImGui::GetIO().DisplaySize.y << "\n";
-  // bool open = true;
+  // std::cout << ImGui::GetIO().DisplaySize.x << " " <<
+  // ImGui::GetIO().DisplaySize.y << "\n"; bool open = true;
   int flags = 0;
   flags |= ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove;
   flags |= ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar;
@@ -78,48 +81,83 @@ void my_window(Mesh** meshes, int len) {
 
   // ImGui::Text("Hello, world %d", 123);
   if (ImGui::TreeNode("test")) {
-      ImGui::Text("heeeellllo");
-      ImGui::TreePop();
+    ImGui::Text("heeeellllo");
+    ImGui::TreePop();
   }
-  ImGuiTreeNodeFlags base_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
+  ImGuiTreeNodeFlags base_flags = ImGuiTreeNodeFlags_OpenOnArrow |
+                                  ImGuiTreeNodeFlags_OpenOnDoubleClick |
+                                  ImGuiTreeNodeFlags_SpanAvailWidth;
   static int selection_mask = 0; //(1 << 2);
   int node_clicked = -1;
 
-
   for (int i = 0; i < len; i++) {
-    Mesh * m = meshes[i];
+    Mesh *m = meshes[i];
     ImGuiTreeNodeFlags node_flags = base_flags;
     const bool is_selected = (selection_mask & (1 << i)) != 0;
     if (is_selected)
-        node_flags |= ImGuiTreeNodeFlags_Selected;
-    bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, "Selectable Node %d", i);
+      node_flags |= ImGuiTreeNodeFlags_Selected;
+    bool node_open =
+        ImGui::TreeNodeEx((void *)(intptr_t)i, node_flags, "Mesh %d", i);
     if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen())
-        node_clicked = i;
+      node_clicked = i;
     if (node_open) {
-        // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_OpenOnArrow",       &base_flags, ImGuiTreeNodeFlags_OpenOnArrow);
-        // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_OpenOnDoubleClick", &base_flags, ImGuiTreeNodeFlags_OpenOnDoubleClick);
-        // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_SpanAvailWidth",    &base_flags, ImGuiTreeNodeFlags_SpanAvailWidth); ImGui::SameLine(); HelpMarker("Extend hit area to all available width instead of allowing more items to be laid out after the node.");
-        // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_SpanFullWidth",     &base_flags, ImGuiTreeNodeFlags_SpanFullWidth);
+      object_inspector_active = true;
+      ImGui::SetNextWindowPos(ImVec2(0, size_y));
+      ImGui::SetNextWindowSize(ImVec2(300, size_y));
+      if (ImGui::Begin("Inspector", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+        // if (ImGui::BeginTable("table1", 3))
+        int j = 0;
+        for (const auto &point : m->points) {
+          // if (ImGui::TreeNode(&point, "point")) {
+          //   ImGui::Text("{ x: %d, y: %d, z: %d }", point.x, point.y, point.z);
+          //   ImGui::TreePop();
+          // }
+          // if (ImGui::TreeNode(&point, "point")) {
+            // static float vec4f[4] = { 0.10f, 0.20f, 0.30f, 0.44f };
+            char label[64];
+            sprintf(label, "##point: %d", j);// static int vec3i[3] = { 10, 20, 30 };
+            ImGui::InputScalarN(label, ImGuiDataType_Float, (void *)&point, 3, NULL, NULL, NULL, ImGuiInputTextFlags_ReadOnly);
+            // ImGui::Text("{ x: %d, y: %d, z: %d }", point.x, point.y, point.z);
+            // ImGui::TreePop();
+          // }
+          j++;
+        }
+        ImGui::End();
+      }
+    }
+    if (node_open) {
+      // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_OpenOnArrow", &base_flags,
+      // ImGuiTreeNodeFlags_OpenOnArrow);
+      // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_OpenOnDoubleClick",
+      // &base_flags, ImGuiTreeNodeFlags_OpenOnDoubleClick);
+      // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_SpanAvailWidth", &base_flags,
+      // ImGuiTreeNodeFlags_SpanAvailWidth); ImGui::SameLine();
+      // HelpMarker("Extend hit area to all available width instead of allowing
+      // more items to be laid out after the node.");
+      // ImGui::CheckboxFlags("ImGuiTreeNodeFlags_SpanFullWidth", &base_flags,
+      // ImGuiTreeNodeFlags_SpanFullWidth);
       // for each point create sub node
-      for (const auto& point : m->points) {
+      // if
+      for (const auto &point : m->points) {
         if (ImGui::TreeNode(&point, "point")) {
-          ImGui::Text("{ x: %d, y: %d, z: %d }",
-                      point.x, point.y, point.z);
+          ImGui::Text("{ x: %f, y: %f, z: %f }", point.x, point.y, point.z);
           ImGui::TreePop();
         }
       }
+      // }
       ImGui::TreePop();
     }
-
   }
-  if (node_clicked != -1)
-  {
-      // Update selection state
-      // (process outside of tree loop to avoid visual inconsistencies during the clicking frame)
-      // if (ImGui::GetIO().KeyCtrl)
-      //     selection_mask ^= (1 << node_clicked);          // CTRL+click to toggle
-      // else //if (!(selection_mask & (1 << node_clicked))) // Depending on selection behavior you want, may want to preserve selection when clicking on item that is part of the selection
-          selection_mask = (1 << node_clicked);           // Click to single-select
+  if (node_clicked != -1) {
+    // Update selection state
+    // (process outside of tree loop to avoid visual inconsistencies during the
+    // clicking frame) if (ImGui::GetIO().KeyCtrl)
+    //     selection_mask ^= (1 << node_clicked);          // CTRL+click to
+    //     toggle
+    // else //if (!(selection_mask & (1 << node_clicked))) // Depending on
+    // selection behavior you want, may want to preserve selection when clicking
+    // on item that is part of the selection
+    selection_mask = (1 << node_clicked); // Click to single-select
   }
 
   // std::cout << open << "\n";
@@ -128,7 +166,7 @@ void my_window(Mesh** meshes, int len) {
 // Main code
 int main(int, char **) {
   // create some random meshes
-  Mesh* meshes[10];
+  Mesh *meshes[10];
   for (int i = 0; i < 10; i++) {
     meshes[i] = create_random_mesh("Mesh", 5);
   }
